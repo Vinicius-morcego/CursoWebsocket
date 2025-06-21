d087cc0684ef3f3b0f61b84992ac27c468373edd
Vinicius Souza <vinicius.souza@novaxs.com.br>
2025-06-21 15:31:28 -0300

    Habilitando e testando o cache na api

---
+++ ws/src/main/java/com/cursochat/ws/cachetest/SpringGenerator.java
@@ -0,0 +1,18 @@
+package com.cursochat.ws.cachetest;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class SpringGenerator {
+    public String buildSimpleString(){
+        return UUID.randomUUID().toString();
+    }
+
+    @Cacheable("cached-string")
+    public String buildCachedString(){
+        return UUID.randomUUID().toString();
+    }
+}
---
+++ ws/src/main/java/com/cursochat/ws/cachetest/TimerLoop.java
@@ -0,0 +1,25 @@
+package com.cursochat.ws.cachetest;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class TimerLoop {
+    @Autowired SpringGenerator generator;
+
+    @PostConstruct
+    void init(){
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                String simple = generator.buildSimpleString();
+                String cached = generator.buildCachedString();
+                System.out.println("Simple: "+simple+" Cache: "+cached);
+            }
+        },2000L, 2000L);
+    }
+}
