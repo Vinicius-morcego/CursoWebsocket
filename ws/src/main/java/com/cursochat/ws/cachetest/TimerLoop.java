eca44279996daa8814fe30992797ccd1d0e98587
Vinicius Souza <vinicius.souza@novaxs.com.br>
2025-06-22 08:56:03 -0300

    Protegendo a conexão websocket com o conceito de tickets

--- ws/src/main/java/com/cursochat/ws/cachetest/TimerLoop.java
+++ ws/src/main/java/com/cursochat/ws/cachetest/TimerLoop.java
@@ -9,17 +9,18 @@ import java.util.TimerTask;
 
 @Component
 public class TimerLoop {
-    @Autowired SpringGenerator generator;
+    /*@Autowired SpringGenerator generator;
 
     @PostConstruct
     void init(){
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
+                return;
                 String simple = generator.buildSimpleString();
                 String cached = generator.buildCachedString();
                 System.out.println("Simple: "+simple+" Cache: "+cached);
             }
         },2000L, 2000L);
-    }
+    }*/
 }
---
+++ ws/src/main/java/com/cursochat/ws/controllers/TicketController.java
@@ -0,0 +1,33 @@
+package com.cursochat.ws.controllers;
+
+import com.cursochat.ws.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("v1/ticket")
+public class TicketController {
+
+    @Autowired
+    private TicketService ticketService;
+
+    @PostMapping
+    public Map<String, String> buildTicket(
+            @RequestHeader(HttpHeaders.AUTHORIZATION)
+            String authorization
+    ){
+        String token =
+                Optional.ofNullable(authorization)
+                        .map(it -> it.replace("Bearer ",""))
+                        .orElse("");
+        String ticket = ticketService.buildAndSaveTicket(token);
+        return Map.of("ticket",ticket);
+
+    }
+}
